82c14dbace11d81af002d6edacdfc9fa8e68d38a
Silvia Patono <silvia.patono@gmail.com>
2019-11-15 11:59:14 +0100

    fix url

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "cocktails#index"
+  resources :cocktails, only: [:new, :create, :index, :show] do
+    resources :doses, only: [:new, :create] # => qui sto creando la dose relativa al cocktail specifico, devo dire a quale cocktail voglio mettere la dose
+  end
+  resources :doses, only: [:destroy] # => qui so a quale cocktail appartiene, quindi non serve nesting
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts "Cleaning database..."
+Ingredient.destroy_all
+puts "Creating ingredients..."
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+drinks = JSON.parse(open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
+drinks["drinks"].each do |drink|
+  Ingredient.create!(name: drink["strIngredient1"])
+end
+
+puts "Finished!"
